08b622a5abe60b67bb3e3bf9b6204e30763a775f
tapasya <tapasyakhs@gmail.com>
2013-08-21 14:01:07 +0530

    Initial Commit

---
+++ sqlite2coredata/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  sqlite2coredata
+//
+//  Created by Tapasya on 21/08/13.
+//  Copyright (c) 2013 Tapasya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
