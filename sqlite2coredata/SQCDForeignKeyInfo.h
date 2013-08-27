205d5d832ff9f51fcf87d085f3696b9b999e6954
Aditya Dasgupta <aditya.d@imaginea.com>
2013-08-27 17:05:58 +0530

    Added code to get xml representation of foreign key relationship

--- sqlite2coredata/SQCDForeignKeyInfo.h
+++ sqlite2coredata/SQCDForeignKeyInfo.h
@@ -14,6 +14,8 @@
 @property (nonatomic, strong) NSString* toSqliteTableName;
 @property (nonatomic, strong) NSString* fromSqliteColumnName;
 @property (nonatomic, strong) NSString* toSqliteColumnName;
+@property (nonatomic, assign) BOOL toMany;
 
+-(NSXMLElement*) xmlRepresentation;
 
 @end
--- sqlite2coredata/SQCDForeignKeyInfo.m
+++ sqlite2coredata/SQCDForeignKeyInfo.m
@@ -10,4 +10,17 @@
 
 @implementation SQCDForeignKeyInfo
 
+-(NSXMLElement*) xmlRepresentation
+{
+    NSXMLElement* childAttr = (NSXMLElement*) [NSXMLNode elementWithName:@"relationship"];
+    [childAttr addAttribute:[NSXMLNode attributeWithName:@"name" stringValue:self.fromSqliteColumnName]];
+    [childAttr addAttribute:[NSXMLNode attributeWithName:@"destinationEntity" stringValue:self.toSqliteTableName]];
+    [childAttr addAttribute:[NSXMLNode attributeWithName:@"toMany" stringValue:(self.toMany ? @"YES":@"NO")]];
+    [childAttr addAttribute:[NSXMLNode attributeWithName:@"inverseName" stringValue:self.toSqliteColumnName]];
+    [childAttr addAttribute:[NSXMLNode attributeWithName:@"inverseEntity" stringValue:self.toSqliteTableName]];
+    [childAttr addAttribute:[NSXMLNode attributeWithName:@"syncable" stringValue:@"YES"]];
+    return childAttr;
+}
+
+
 @end
